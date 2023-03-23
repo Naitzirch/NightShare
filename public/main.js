b44d5050b9939e3cee4e665bb108104738341162
Naitzirch <naitzirch@gmail.com>
2023-03-23 23:27:55 +0100

    changed video constraints

--- public/main.js
+++ public/main.js
@@ -122,7 +122,7 @@ capture.addEventListener("click", async () => {
 
     const constraints = {
         video: {
-            frameRate: { exact: 60},
+            frameRate: 60,
             width: { exact: 1920 },
             height: { exact: 1080 },
             cursor: "always"
